625b5a1bb99bdde397593013a200e82557457f05
soulaimane10 <soulaimane861@gmail.com>
2020-06-04 19:51:20 +0200

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionStockProductsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class ServeurGestionStockProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProductsApplication.class, args);
+	}
+
+}
